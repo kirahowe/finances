0716301ff19437b53c88870c01fa870e5482fbe0
Kira Howe <email@kirahowe.com>
2025-11-18 22:46:52 -0400

    Add inline category selection

--- resources/public/js/transactions.cljs
+++ resources/public/js/transactions.cljs
@@ -18,6 +18,22 @@
   (fn [db [_ page-size]]
     (assoc-in db [:transactions-table :page-size] page-size)))
 
+(rf/reg-event-db
+  ::set-editing-transaction
+  (fn [db [_ tx-id]]
+    (assoc-in db [:transactions-table :editing-tx-id] tx-id)))
+
+(rf/reg-event-db
+  ::update-transaction-category-optimistic
+  (fn [db [_ tx-id category]]
+    (let [transactions (:transactions db)
+          updated-txs (mapv (fn [tx]
+                              (if (= (:db/id tx) tx-id)
+                                (assoc tx :transaction/category category)
+                                tx))
+                            transactions)]
+      (assoc db :transactions updated-txs))))
+
 ;; Subscriptions
 (rf/reg-sub
   ::transactions
@@ -33,6 +49,11 @@
   ::page-size
   (fn [db _]
     (get-in db [:transactions-table :page-size] 20)))
+
+(rf/reg-sub
+  ::editing-transaction
+  (fn [db _]
+    (get-in db [:transactions-table :editing-tx-id])))
 
 ;; API calls
 (defn fetch-json [url on-success on-error]
@@ -67,6 +88,44 @@
         (rf/dispatch [:finance-aggregator.ui/set-loading :transactions false])
         (rf/dispatch [:finance-aggregator.ui/set-error (str error)])))
     {}))
+
+(rf/reg-event-fx
+  ::update-transaction-category
+  (fn [{:keys [db]} [_ tx-id category-id]]
+    ;; Find the category object
+    (let [category (when category-id
+                     (first (filter #(= (:db/id %) category-id)
+                                   (get-in db [:categories]))))
+          ;; Store original transaction for rollback
+          original-tx (first (filter #(= (:db/id %) tx-id) (:transactions db)))]
+      ;; Optimistic update
+      (rf/dispatch [::update-transaction-category-optimistic tx-id category])
+      (rf/dispatch [::set-editing-transaction nil])
+
+      ;; Make API call
+      (-> (js/fetch (str "/api/transactions/" tx-id "/category")
+                    #js {:method "PUT"
+                         :headers #js {"Content-Type" "application/json"}
+                         :body (js/JSON.stringify #js {:categoryId category-id})})
+          (.then (fn [response]
+                   (if (.-ok response)
+                     (.json response)
+                     (throw (js/Error. (str "HTTP error: " (.-status response)))))))
+          (.then (fn [js-data]
+                   (let [data (js->clj js-data :keywordize-keys true)]
+                     (when-not (:success data)
+                       ;; Rollback on failure
+                       (rf/dispatch [::update-transaction-category-optimistic
+                                    tx-id
+                                    (:transaction/category original-tx)])
+                       (rf/dispatch [:finance-aggregator.ui/set-error (:error data)])))))
+          (.catch (fn [error]
+                    ;; Rollback on error
+                    (rf/dispatch [::update-transaction-category-optimistic
+                                 tx-id
+                                 (:transaction/category original-tx)])
+                    (rf/dispatch [:finance-aggregator.ui/set-error (str error)]))))
+      {})))
 
 ;; Helper functions
 (defn format-amount [amount]
@@ -114,7 +173,48 @@
                           (when-let [cat (aget row "transaction/category")]
                             (or (aget cat "category/name") "Uncategorized")))
             :header "Category"
-            :cell (fn [info] (or (.getValue info) "Uncategorized"))}])
+            :cell (fn [info]
+                    (let [createElement (.-createElement React)
+                          row-data (.-original (.-row info))
+                          tx-id (aget row-data "db/id")
+                          meta (-> info .-table .-options .-meta)
+                          editing-tx-id (when meta (.-editingTxId meta))
+                          categories (when meta (.-categories meta))
+                          on-edit-click (when meta (.-onEditClick meta))
+                          on-category-select (when meta (.-onCategorySelect meta))
+                          on-cancel (when meta (.-onCancel meta))
+                          is-editing (= tx-id editing-tx-id)
+                          current-category (aget row-data "transaction/category")
+                          current-category-id (when current-category (aget current-category "db/id"))
+                          category-name (or (.getValue info) "Uncategorized")]
+
+                      (if is-editing
+                        ;; Show dropdown when editing
+                        (createElement "select"
+                          #js {:autoFocus true
+                               :defaultValue (or current-category-id "")
+                               :onChange (fn [e]
+                                          (let [val (-> e .-target .-value)]
+                                            (when on-category-select
+                                              (on-category-select tx-id (if (= val "") nil (js/parseInt val))))))
+                               :onBlur (fn [_] (when on-cancel (on-cancel)))
+                               :style #js {:width "100%"}}
+                          (into-array
+                            (concat
+                              [(createElement "option" #js {:value "" :key "none"} "Uncategorized")]
+                              (when categories
+                                (map (fn [cat]
+                                       (let [cat-id (aget cat "db/id")
+                                             cat-name (aget cat "category/name")]
+                                         (createElement "option" #js {:value cat-id :key cat-id} cat-name)))
+                                     categories)))))
+                        ;; Show category name when not editing
+                        (createElement "span"
+                          #js {:onClick (fn [_] (when on-edit-click (on-edit-click tx-id)))
+                               :style #js {:cursor "pointer"
+                                          :textDecoration "underline"
+                                          :color "#007aff"}}
+                          category-name))))}])
 
 ;; Create a pure React component directly in JavaScript
 ;; This allows us to use React hooks properly
@@ -124,6 +224,11 @@
       (let [data (.-data props)
             page-index (.-pageIndex props)
             page-size (.-pageSize props)
+            categories (.-categories props)
+            editing-tx-id (.-editingTxId props)
+            on-edit-click (.-onEditClick props)
+            on-category-select (.-onCategorySelect props)
+            on-cancel (.-onCancel props)
 
             ;; React hooks - these work because this is a real React function component
             sorting-state (useState #js [])
@@ -137,7 +242,12 @@
                          :state #js {:sorting sorting}
                          :onSortingChange set-sorting
                          :getCoreRowModel (getCoreRowModel)
-                         :getSortedRowModel (getSortedRowModel)})
+                         :getSortedRowModel (getSortedRowModel)
+                         :meta #js {:categories categories
+                                    :editingTxId editing-tx-id
+                                    :onEditClick on-edit-click
+                                    :onCategorySelect on-category-select
+                                    :onCancel on-cancel}})
 
             ;; Get rows and apply pagination
             all-rows (-> table .getRowModel .-rows)
@@ -189,9 +299,12 @@
         page-index @(rf/subscribe [::page-index])
         page-size @(rf/subscribe [::page-size])
         loading? @(rf/subscribe [:finance-aggregator.ui/loading? :transactions])
+        categories @(rf/subscribe [:finance-aggregator.ui.categories/categories])
+        editing-tx-id @(rf/subscribe [::editing-transaction])
 
         ;; Convert ClojureScript data to JavaScript for TanStack Table
         data (to-array (map #(clj->js % :keyword-fn (fn [k] (str (namespace k) "/" (name k)))) transactions))
+        categories-js (to-array (map #(clj->js % :keyword-fn (fn [k] (str (namespace k) "/" (name k)))) categories))
 
         ;; Pagination
         total-rows (count transactions)
@@ -215,7 +328,15 @@
         ;; Use the React component
         [:> TransactionsTable {:data data
                                :pageIndex page-index
-                               :pageSize page-size}]
+                               :pageSize page-size
+                               :categories categories-js
+                               :editingTxId editing-tx-id
+                               :onEditClick (fn [tx-id]
+                                             (rf/dispatch [::set-editing-transaction tx-id]))
+                               :onCategorySelect (fn [tx-id category-id]
+                                                  (rf/dispatch [::update-transaction-category tx-id category-id]))
+                               :onCancel (fn []
+                                          (rf/dispatch [::set-editing-transaction nil]))}]
 
         ;; Pagination controls
         [:div {:style {:margin-top "15px" :display "flex" :gap "10px" :align-items "center"}}
